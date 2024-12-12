fa25df8fc798e12ac23f7f249f76b302f07a7a8b
Boris Lau <boris@techie.im>
2024-12-11 20:03:43 -0800

    Added VR mode

--- webxr-client/src/App.tsx
+++ webxr-client/src/App.tsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
+import { createXRStore, XR } from '@react-three/xr'
+
+const xrStore = createXRStore();
 
 function App() {
   return (
     <>
       <Canvas>
+        <XR store={xrStore}>
         <directionalLight color="white" position={[0, 0, 5]} />
-        <mesh>
+        <mesh position={[0, 0, -10]}>
           <sphereGeometry args={[1, 64, 64]} />
           <meshStandardMaterial color="blue" />
         </mesh>
+        </XR>
       </Canvas>
       <h1>A simple WebXR demo.</h1>
+      <button onClick={() => xrStore.enterVR()}>Enter VR</button>
     </>
   )
 }
